b6f584568a8e11aede11bcb89bb037a61c7d912e
amritsagoo91 <amritsagoo91@gmail.com>
2024-10-04 14:21:32 -0700

    exercise 2.2 and 2.3*

--- part2/part2-a/src/App.jsx
+++ part2/part2-a/src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from 'react'
 
-const Part=({part})=><p>{part.name} {part.exercises}</p>
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
-
-const Header=({name})=><h1>{name}</h1>
-const Content=({parts})=>{
-  return(
+const Header = ({ name }) => <h1>{name}</h1>
+const Content = ({ parts }) => {
+  return (
     <>
-    {
-      parts.map((part)=><Part key={part.id} part={part}/>)
-    }
+      {
+        parts.map((part) => <Part key={part.id} part={part} />)
+      }
     </>
   )
 }
 
-const Course=({course})=>{
-  return(<>
-  <Header name={course.name}/>
-  <Content parts={course.parts}/>
+const Total = ({ parts }) => {
+  return (
+    <h4>
+      Total of {parts.reduce((sum, part) => sum += part.exercises, 0)} exercises
+    </h4>
+  )
+}
+
+const Course = ({ course }) => {
+  return (<>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </>)
 }
 
